03f338d6da1a260b4f47146bb6271f51a2e65a6e
ThanathornJung <thanathorn.workspace@gmail.com>
2024-03-19 20:39:33 +0700

    Finished

---
+++ MyPortfolio/src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,28 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Hi! I'm Peak, a game programmer and designer from Thailand, and I've
+            graduated from Bangkok University studying Information Technology
+            and Innovation, major Games and Interactive Media.
+          </p>
+          <p className="hero--section-description">
+            Here you can see some of the projects I've been working on. I have a
+            strong perseverance for learning new things and taking on
+            challenges, and I always like to put some extra time in polishing my
+            creations, to make sure the creative vision is achieved in the best
+            possible way. I've always been extremely curious in the technical
+            side of games, and sometimes this curiosity has led me to projects I
+            won't be able to formally mention here. Sometimes I also make
+            websites, like this one. If you have any questions please feel free
+            to contact me :)
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
