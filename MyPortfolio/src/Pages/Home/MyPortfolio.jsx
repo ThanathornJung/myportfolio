185cb1640bb11e2ebcb052bc3350fe09ca6fcb17
ThanathornJung <thanathorn.workspace@gmail.com>
2024-03-27 19:44:18 +0700

    FixedLink

--- MyPortfolio/src/Pages/Home/MyPortfolio.jsx
+++ MyPortfolio/src/Pages/Home/MyPortfolio.jsx
@@ -10,19 +10,72 @@ export default function MyPortfolio() {
         </div>
       </div>
       <div className="portfolio--section--container">
-        {data?.portfolio?.map((item, index) => (
-          <div key={index} className="portfolio--section--card">
-            <div className="portfolio--section--img">
-              <img src={item.src} alt="Placeholder" />
+        <div className="portfolio--section--card">
+          <a href="https://proawsaengkatc.itch.io/suis-its-so-close">
+            <div class="portfolio--section--img">
+              <img src="./img/placeholder-image.png" alt="Placeholder" />
             </div>
-            <div className="portfolio--section--card--content">
+            <div class="portfolio--section--card--content">
               <div>
-                <h3 className="portfolio--section--title">{item.title}</h3>
-                <p className="text-md">{item.description}</p>
+                <h3 class="portfolio--section--title">SUIS IT'S SO CLOSE</h3>
+                <p class="text-md">
+                  This is choice selection game that educates players about the prevention of Streptococcus suis Infection
+                </p>
               </div>
+              <p class="text-sm portfolio--link">
+                View Doc
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 20 19"
+                  fill="none"
+                >
+                  <path
+                    d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
+                    stroke="currentColor"
+                    stroke-width="2.66667"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </p>
             </div>
-          </div>
-        ))}
+          </a>
+        </div>
+        <div className="portfolio--section--card">
+          <a href="https://github.com/ThanathornJung/CookingMania.git">
+            <div class="portfolio--section--img">
+              <img src="./img/placeholder-image-1.png" alt="Placeholder" />
+            </div>
+            <div class="portfolio--section--card--content">
+              <div>
+                <h3 class="portfolio--section--title">CookingMania</h3>
+                <p class="text-md">
+                  This game is Overcook clone, That I made to practice multiplayer system
+                </p>
+              </div>
+              <p class="text-sm portfolio--link">
+                View in GitHub
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 20 19"
+                  fill="none"
+                >
+                  <path
+                    d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
+                    stroke="currentColor"
+                    stroke-width="2.66667"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </p>
+            </div>
+          </a>
+        </div>
       </div>
     </section>
   );
